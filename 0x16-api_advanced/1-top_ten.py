222eaff8392d6970b4eeb0d898d962066f330278
Faithlicious <violetshiko15@gmail.com>
2024-08-14 13:41:17 -0700

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""function that queries the reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit."""
-
-
+"""
+Query the top ten hot posts listed for a given subreddit
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """function that queries the reddit API and prints the
-        titles of the first 10 hot posts listed for
-        a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """
+    the top ten queries
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'My-User-Agent'}
     try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
+        respo = requests.get(url, headers=headers, allow_redirects=False)
+        if respo.status_code == 200:
+            data = respo.json()
+            posts = data.get('data').get('children')
+            for post in posts:
+                print(post.get('data').get('title'))
+        return None
     except Exception:
-        print("None")
+        return None
